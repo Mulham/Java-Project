da3c589047d9b2ba9be4bd2049d03b87711da88c
mulham <mulham94@gmail.com>
2021-03-03 00:28:59 +0300

    initialize

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,7 @@
-module INF202 {
-    requires javafx.graphics;
-    requires javafx.controls;
+module java.project {
     requires javafx.fxml;
-    requires java.sql;
-    requires com.jfoenix;
-    requires aspose.cells;
-    requires java.desktop;
+    requires javafx.controls;
+    requires javafx.graphics;
+    exports sample.controller;
     opens sample;
 }
